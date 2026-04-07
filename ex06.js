55ffdec5a1d7eef271fc3e450b09323dbc322915
sosastacy1 <stacysosa023@gmail.com>
2026-04-07 16:10:15 -0300

    feat: Subindo exercicio 6

---
+++ ex06.js
@@ -0,0 +1,15 @@
+// const readline = require('readline-sync')
+
+// let meta = 40
+
+// let horasEstudadas = readline.questionFloat("Informe quantas horas você estudou: ")
+
+// let diferenca = meta - horasEstudadas
+
+// console.log("========= RESULTADO ========")
+
+// if(horasEstudadas >= meta){
+//     console.log("Parabéns!! Você alcançou a meta")
+// }else{
+//     console.log(`Falta ${diferenca} horas estudadas`)
+// }
