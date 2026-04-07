9af58be411922d35351180812b84c52071a4c7e6
sosastacy1 <stacysosa023@gmail.com>
2026-04-07 16:10:00 -0300

    feat: Subindo exercicio 5

---
+++ ex05.js
@@ -0,0 +1,15 @@
+// const readline = require('readline-sync')
+
+// let objetivo = 500
+
+// let dinheiroGuardado = readline.questionFloat("Informe o valor guardado: ")
+
+// let diferenca = objetivo - dinheiroGuardado
+
+// console.log("========= RESULTADO =========")
+
+// if(dinheiroGuardado >= objetivo){
+//     console.log("Parabéns!! Você atingiu a meta")
+// }else{
+//     console.log(`Faltaram ${diferenca} para atingir o objetivo`)
+// }
