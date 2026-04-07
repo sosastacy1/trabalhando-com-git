6e8ca2e4101e3e2ddda7e345f8ea98f033b041ff
sosastacy1 <stacysosa023@gmail.com>
2026-04-07 16:12:19 -0300

    feat: Subindo exercicio 7

---
+++ ex07.js
@@ -0,0 +1,11 @@
+// const readline = require('readline-sync')
+
+// let nota = readline.questionFloat("Escreva a nota que você tirou: ")
+
+// console.log("======== RESULTADO =========")
+
+// if(nota >= 7){
+//     console.log("Aprovado!")
+// }else{
+//     console.log("Reprovado")
+// }
