e5df8b38359aaf4154acf7bcfe6768ea2c46ca78
sosastacy1 <stacysosa023@gmail.com>
2026-04-09 09:05:23 -0300

    feat: Subindo exercicio 1

--- ex01.js
+++ ex01.js
@@ -1,7 +1,4 @@
-// const readline = require ("readline-sync")
-// let nome = readline.question("Digite o seu nome: Stacy")
-// console.log(`Seu nome é: ${nome}`)
-
+const readline = require("readline-sync")
 
 // Seu objetivo é criar um programa que solicite ao usuário as seguintes informações:
 // Nome do aluno
@@ -9,17 +6,13 @@
 // Idade
 
 
-// Importa a biblioteca readline-sync
-// const readline = require ("readline-sync")
+// Solicita os dados dos usuários
+let nome = readline.question("Informe seu nome: ")
+let curso = readline.question("Informe o curso: ")
+let idade = readline.questionInt("Informe sua idade: ")
 
-
-// // Solicita os dados dos usuários
-// let nome = readline.question("Informe seu nome: ")
-// let curso = readline.question("Informe o curso: ")
-// let idade = readline.questionInt("Informe sua idade: ")
-
-// console.log("========== SISTEMA DE CADASTRO ============")
-// console.log(`Nome do usuário: ${nome}`)
-// console.log(`Curso do usuário: ${curso}`)
-// console.log(`Idade do usuário: ${idade}`)
+console.log("========== SISTEMA DE CADASTRO ============")
+console.log(`Nome do usuário: ${nome}`)
+console.log(`Curso do usuário: ${curso}`)
+console.log(`Idade do usuário: ${idade}`)
 
