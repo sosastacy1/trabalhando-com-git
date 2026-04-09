6936c0bb411cf635a97d2e7e2c14dd8055a88541
sosastacy1 <stacysosa023@gmail.com>
2026-04-09 09:11:44 -0300

    feat: Subindo exercicio 4

--- ex04.js
+++ ex04.js
@@ -1,17 +1,17 @@
-// const readline = require('readline-sync')
+const readline = require('readline-sync')
 
-// let meta = 10000
+let meta = 10000
 
-// let passos = readline.questionInt("Quantos passos você deu hoje?")
+let passos = readline.questionInt("Quantos passos você deu hoje? ")
 
-// let diferenca = meta - passos
+let diferenca = meta - passos
 
-// console.log("============= RESULTADO ============")
+console.log("============= RESULTADO ============")
 
-// if(passos >= meta){
-//     console.log ("Parabéns! Vocễ atingiu a meta")
-//     console.log(`Você passou ${diferenca} passos da meta`)
-// }else{
-//     console.log(`Fataram ${diferenca} passos`)
-// }
+if(passos >= meta){
+    console.log ("Parabéns! Vocễ atingiu a meta")
+    console.log(`Você passou ${diferenca} passos da meta`)
+}else{
+    console.log(`Fataram ${diferenca} passos`)
+}
 
