c7a987ed2dfd976790cd791caf2cdc0d277e2f1c
sosastacy1 <stacysosa023@gmail.com>
2026-04-09 08:34:56 -0300

    feat: Subindo exercicio 10

--- ex10.js
+++ ex10.js
@@ -1,14 +1,13 @@
 const readline = require('readline-sync')
 
-let paginasTotal = 300
+let livroTotal = 300
+        console.log("------------------")
+        console.log("|   BIBLIOTECA   |")
+        console.log("------------------")
 
-console.log("------------------")
-console.log("|   BIBLIOTECA   |")
-console.log("------------------")
+let paginasLidas = readline.questionInt ("Informe quantas paginas voce leu: ")
 
-let paginasLidas = readline.questionInt("Informe quantas paginas voce leu: ")
-
-let diferenca = paginasTotal - paginasLidas
+let diferenca =  livroTotal - paginasLidas
 
 console.log("----------RESULTADO---------")
-console.log(`Você já leu ${Math.round((paginasTotal/paginasLidas) * 100)}% do livro, faltam ${diferenca} páginas para completar sua leitura!`)
+       console.log(`Você já leu ${Math.round((paginasLidas/livroTotal)* 100)}% do livro, faltam ${diferenca} páginas para terminar!`)
