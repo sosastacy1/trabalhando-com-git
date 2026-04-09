c44a8a70535c7987e834a84507316ecaa46d4c0b
sosastacy1 <stacysosa023@gmail.com>
2026-04-09 09:10:29 -0300

    feat: Subindo exercicio 3

--- ex03.js
+++ ex03.js
@@ -1,11 +1,11 @@
-// const readline = require ("readline-sync")
+const readline = require ("readline-sync")
 
-// let nome = readline.question("Nome do jogador: ")
-// let nomeDoJogoFavorito = readline.question("Informe seu jogo favorito: ")
-// let PontuaçãoFinal = readline.questionFloat("Informe a pontuação atual: ")
+let nome = readline.question("Nome do jogador: ")
+let nomeDoJogoFavorito = readline.question("Informe seu jogo favorito: ")
+let PontuaçãoFinal = readline.questionFloat("Informe a pontuação atual: ")
 
-// console.log ("============= RESUMO DO PLACAR ============")
-// console.log(`Nome do jogador: ${nome}`)
-// console.log(`Jogo favorito: ${nomeDoJogoFavorito}`)
-// console.log(`Pontuação final: ${PontuaçãoFinal}`)
+console.log ("============= RESUMO DO PLACAR ============")
+console.log(`Nome do jogador: ${nome}`)
+console.log(`Jogo favorito: ${nomeDoJogoFavorito}`)
+console.log(`Pontuação final: ${PontuaçãoFinal}`)
 
