67816e4341022bfb497560a9caa58af0bf6625e0
sosastacy1 <stacysosa023@gmail.com>
2026-04-09 08:59:38 -0300

    feat: Subindo exercicio 2

--- ex02.js
+++ ex02.js
@@ -1,12 +1,12 @@
 // importando a biblioteca
-// const readline = require ("readline-sync")
+const readline = require ("readline-sync")
 
-// let nome = readline.question("Informe seu nome: ")
-// let nomeDoPrato = readline.question("Informe o nome do prato: ")
-// let valorDoPrato = readline.questionFloat("Informe o valor do prato: ")
+let nome = readline.question("Informe seu nome: ")
+let nomeDoPrato = readline.question("Informe o nome do prato: ")
+let valorDoPrato = readline.questionFloat("Informe o valor do prato: ")
 
-// console.log ("============= RESUMO DO PEDIDO ============")
-// console.log(`Cliente: ${nome}`)
-// console.log(`Prato Escolhido: ${nomeDoPrato}`)
-// console.log(`Valor: ${valorDoPrato}`)
+console.log ("============= RESUMO DO PEDIDO ============")
+console.log(`Cliente: ${nome}`)
+console.log(`Prato Escolhido: ${nomeDoPrato}`)
+console.log(`Valor: ${valorDoPrato}`)
 
